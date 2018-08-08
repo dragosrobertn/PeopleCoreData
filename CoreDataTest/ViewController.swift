fbf85700dbd7764427103de79dc711da32fca7ec
Dragos-Robert Neagu <hello@dragosneagu.com>
2018-08-08 22:09:35 +0100

    Add AppIcon, delete records from Container using swipes, change app to NavigationController.

--- CoreDataTest/ViewController.swift
+++ CoreDataTest/ViewController.swift
@@ -10,13 +10,12 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-    
     var people = [Person]()
     
+    @IBOutlet weak var tableView: UITableView!
+    
     @IBAction func addElement(_ sender: Any) {
-        let alert = UIAlertController(title: "Add person", message: nil, preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add Person", message: nil, preferredStyle: .alert)
         
         alert.addTextField(configurationHandler: {(textfield) in
             textfield.placeholder = "Name"
@@ -35,15 +34,13 @@ class ViewController: UIViewController {
             person.name = name
             person.age = Int16(age!)!
             
-            PersistenceService.saveContext()
             self.people.append(person)
-            self.tableView.reloadData()
+            
+            self.saveContextAndReloadTable()
         })
         
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
-
     }
     
     override func viewDidLoad() {
@@ -58,15 +55,19 @@ class ViewController: UIViewController {
             print("Error retrieveing from Persistence Service")
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
 
 extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    fileprivate func saveContextAndReloadTable() {
+        PersistenceService.saveContext()
+        self.tableView.reloadData()
+    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -84,8 +85,15 @@ extension ViewController : UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        return UISwipeActionsConfiguration(actions: [UIContextualAction.init(style: .destructive, title: "Delete", handler: {_,_,_ in
+            PersistenceService.context.delete(self.people[indexPath.row])
+            self.people.remove(at: indexPath.row)
+            self.saveContextAndReloadTable()
+        })])
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         let alert = UIAlertController(title: "Change person", message: nil, preferredStyle: .alert)
         
         alert.addTextField(configurationHandler: {(textfield) in
@@ -100,19 +108,15 @@ extension ViewController : UITableViewDataSource, UITableViewDelegate {
         let action = UIAlertAction(title: "Save", style: .default, handler: {(_) in
             let name = alert.textFields?.first!.text!
             let age = alert.textFields?.last!.text!
-      
-
-            self.people[indexPath.row].name = name
-            self.people[indexPath.row].age = Int16(age!)!
             
-            PersistenceService.saveContext()
-            self.tableView.reloadData()
+            self.people[indexPath.row].name = name
+            self.people[indexPath.row].age = Int16(age!)! // This should be handled properly!
+            
+            self.saveContextAndReloadTable()
         })
         
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
-        
     }
     
 }
